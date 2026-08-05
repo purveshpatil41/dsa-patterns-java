dfce36a3442d9de797b6b6e01d88cf835c379795
Purvesh Patil <purveshpatil442@gmail.com>
2026-08-05 16:56:28 +0530

    Time: 0 ms (100%), Space: 44.2 MB (60.29%) - LeetHub

---
+++ 2079-watering-plants/2079-watering-plants.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int wateringPlants(int[] plants, int capacity) {
+        int steps = 0;
+        int water = capacity;
+        for(int i = 0; i<plants.length; i++){
+
+            if(water < plants[i]){
+                steps += 2 * i;
+                water = capacity;
+            }
+            steps++;
+
+            water -= plants[i];
+        }
+        return steps;
+    }
+}
